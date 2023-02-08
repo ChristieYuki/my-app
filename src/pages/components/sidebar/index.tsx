c88381255ea0cd3f24d330c77d4fcecb6ee0950c
christie.mashima <christie.mashima@gerdau.com>
2023-02-08 10:55:03 -0300

    next and chakra

---
+++ src/pages/components/sidebar/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Sidebar(){
+return (
+    <div>
+      <aside>
+        <nav>
+          <ul>
+            <li>Linha 1</li>
+            <li>Linha 2</li>
+            <li>Linha 3</li>              
+          </ul>
+        </nav>
+      </aside>
+ </div>
+  );
+};
