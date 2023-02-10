2cde5af32ef44b79b9ee5f58f195b45e5ec1d6aa
christie.mashima <christie.mashima@gerdau.com>
2023-02-10 10:25:03 -0300

    another sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import { NextPage } from 'next';
 import { sleep } from './utils/sleep';
 import React from 'react';
 import CallToActionWithAnnotation from './layout/Hero';
-
+import SimpleSidebar from './components/SimpleSidebar';
+const cantent = "main content";
 //Melhor para tipar!
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Hello</h1>
       <Sidebar />
+      <SimpleSidebar children={cantent} />
       {/* <CallToActionWithAnnotation /> */}
     </div>
   )
